8698127646140fb0473e7654db9f2889938e5633
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-01-13 22:45:06 +0100

    initial commit

---
+++ hooks/index.ts
@@ -0,0 +1,92 @@
+import { useCallback, useMemo, useState } from "react"
+
+export class CancelError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = "CancelError"
+  }
+}
+
+export interface UseRequestOptions<Data> {
+  initialPath: string;
+  initialUrl: string;
+  initialQueries: Record<string, string>;
+  initialData: Data;
+  initialOptions: RequestInit;
+  resolver: (response: Response) => Promise<Data>
+}
+
+export const useRequest = <Data>({initialUrl, initialPath, initialQueries, initialData, initialOptions, resolver}: UseRequestOptions<Data>) => {
+  const [url, setUrl] = useState(initialUrl)
+  const [path, setPath] = useState(initialPath)
+  const [queries, setQueries] = useState(initialQueries)
+  const [options, setOptions] = useState<RequestInit>(initialOptions)
+  const [error, setError] = useState<Error | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [abortController, setAbortController] = useState(new AbortController())
+  const [data, setData] = useState(initialData)
+
+  const stringifiedQueries = useMemo(() => {
+    const queryEntries = Object.entries(queries)
+
+    if (queryEntries.length === 0) {
+      return ""
+    }
+
+    console.log(`?${new URLSearchParams(queryEntries).toString()}`)
+
+    return `?${new URLSearchParams(queryEntries).toString()}`
+  }, [queries])
+
+  const cancel = useCallback(() => {
+    abortController.abort()
+  }, [abortController])
+
+  const request = useCallback(() => {
+    const newAbortController = new AbortController()
+
+    setAbortController(newAbortController)
+    setError(null)
+    setLoading(true)
+
+    const endpoint = new URL(`${path}${stringifiedQueries}`, url)
+    console.log({endpoint})
+
+    fetch(endpoint, {
+      ...options,
+      signal: newAbortController.signal
+    }).then(resolver).then(newData => {
+      setData(newData)
+    }).catch(error => {
+      if (error instanceof Error && error.name === "AbortError") {
+        setError(new CancelError("Request has been canceled"))
+        return
+      }
+
+      setError(error)
+    }).finally(() => {
+      setLoading(false)
+    })
+  }, [options, url, stringifiedQueries, path])
+
+  return {
+    stringifiedQueries,
+    queries,
+    path,
+    url,
+    data,
+    error,
+    loading,
+    setQueries,
+    setUrl,
+    setPath,
+    setData,
+    setError,
+    setLoading,
+    abortController,
+    setAbortController,
+    setOptions,
+    cancel,
+    request
+  }
+}
