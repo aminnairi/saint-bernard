712ede6a264f7149b16bd2239dca82510ad57c5d
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-01-14 21:07:36 +0100

    added react router dom

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import { Main } from "./main"
 
 const rootElement = document.getElementById("root")
@@ -11,5 +12,7 @@ if (!rootElement) {
 const root = createRoot(rootElement)
 
 root.render(
-  <Main />
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>
 )
