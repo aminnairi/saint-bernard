eb25d461b3932abafec83f1f1d6c56509806c806
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-06-29 15:24:20 +0200

    updated tests according to the recent changes

--- hooks/index.test.tsx
+++ hooks/index.test.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { render } from "@testing-library/react"
 import { describe, test, expect, vi } from "vitest"
-import { CancelError, useRequest } from "."
+import { CancelError, useStatefulRequest, useStatelessRequest } from "."
 
 describe("CancelError", () => {
   test("It should return an error instance", () => {
@@ -13,18 +13,23 @@ describe("CancelError", () => {
   })
 })
 
-describe("useRequest", () => {
+describe("useStatefulRequest", () => {
   test("It should work without state nor resolver", () => {
     const spy = vi.spyOn(window, "fetch");
 
     spy.mockImplementation(() => Promise.resolve(new Response()))
 
     const Main = () => {
-      const { request } = useRequest()
+      const { request } = useStatefulRequest({
+        initialState: null
+      })
 
       useEffect(() => {
         request({
           url: "https://jsonplaceholder.typicode.com/users",
+          onResponse: async () => {
+            return null;
+          }
         })
       }, [request])
 
@@ -46,7 +51,7 @@ describe("useRequest", () => {
     spy.mockImplementation(() => Promise.resolve(new Response(JSON.stringify([{ id: 1 }]))))
 
     const Main = () => {
-      const { state, request } = useRequest({
+      const { state, request } = useStatefulRequest({
         initialState: [],
       })
 
@@ -89,15 +94,15 @@ describe("useRequest", () => {
     })
 
     const Main = () => {
-      const { error, cancel, request } = useRequest({
-        initialState: []
+      const { error, cancel, request } = useStatefulRequest({
+        initialState: null
       })
 
       useEffect(() => {
         request({
           url: "https://jsonplaceholder.typicode.com/users",
-          onResponse: response => {
-            return response.json();
+          onResponse: async () => {
+            return null;
           }
         })
       }, [request])
@@ -134,15 +139,15 @@ describe("useRequest", () => {
     })
 
     const Main = () => {
-      const { error, request } = useRequest({
-        initialState: []
+      const { error, request } = useStatefulRequest({
+        initialState: null
       })
 
       useEffect(() => {
         request({
           url: "https://jsonplaceholder.typicode.com/users",
-          onResponse: async response => {
-            return response.json();
+          onResponse: async () => {
+            return null;
           }
         })
       }, [request])
@@ -163,6 +168,160 @@ describe("useRequest", () => {
     await new Promise(resolve => setTimeout(resolve, 1))
 
     expect(container.innerText).toEqual("An error occurred")
+
+    spy.mockClear()
+  })
+})
+
+describe("useStatelessRequest", () => {
+  test("It should work without state nor resolver", () => {
+    const spy = vi.spyOn(window, "fetch");
+
+    spy.mockImplementation(() => Promise.resolve(new Response()))
+
+    const Main = () => {
+      const { request } = useStatelessRequest()
+
+      useEffect(() => {
+        request({
+          url: "https://jsonplaceholder.typicode.com/users"
+        })
+      }, [request])
+
+      return (
+        <p>Success</p>
+      )
+    }
+
+    const { container } = render(<Main />)
+
+    expect(container.innerText).toEqual("Success")
+
+    spy.mockClear();
+  });
+
+  test("It should return a cancel error", async () => {
+    const spy = vi.spyOn(window, "fetch")
+
+    spy.mockImplementation(async (_url, options) => {
+      await new Promise(resolve => {
+        setTimeout(resolve, 1000);
+      })
+
+      if (options?.signal?.aborted) {
+        const error = new Error("AbortError")
+        error.name = "AbortError"
+        throw error
+      }
+
+      return Promise.resolve(new Response())
+    })
+
+    const Main = () => {
+      const { error, cancel, request } = useStatelessRequest()
+
+      useEffect(() => {
+        request({
+          url: "https://jsonplaceholder.typicode.com/users"
+        })
+      }, [request])
+
+      useEffect(() => {
+        cancel()
+      }, [cancel])
+
+      if (error && error instanceof CancelError) {
+        return (
+          <p>Canceled by the user</p>
+        )
+      }
+
+      return (
+        <p>Success</p>
+      )
+    }
+
+    const { container } = render(<Main />)
+
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    expect(container.innerText).toEqual("Canceled by the user")
+
+    spy.mockClear()
+  })
+
+  test("It should return an error", async () => {
+    const spy = vi.spyOn(window, "fetch")
+
+    spy.mockImplementation(async () => {
+      throw new Error("An error occurred")
+    })
+
+    const Main = () => {
+      const { error, request } = useStatelessRequest()
+
+      useEffect(() => {
+        request({
+          url: "https://jsonplaceholder.typicode.com/users"
+        })
+      }, [request])
+
+      if (error) {
+        return (
+          <p>{error.message}</p>
+        )
+      }
+
+      return (
+        <p>Success</p>
+      )
+    }
+
+    const { container } = render(<Main />)
+
+    await new Promise(resolve => setTimeout(resolve, 1))
+
+    expect(container.innerText).toEqual("An error occurred")
+
+    spy.mockClear()
+  })
+
+  test("It should work with an external state", async () => {
+    const spy = vi.spyOn(window, "fetch")
+
+    spy.mockImplementation(() => Promise.resolve(new Response(JSON.stringify(123))))
+
+    const Main = () => {
+      const [state, setState] = useState("");
+      const { error, request } = useStatelessRequest()
+
+      useEffect(() => {
+        request({
+          url: "https://jsonplaceholder.typicode.com/users",
+          onResponse: async response => {
+            const text = await response.text();
+
+            setState(text);
+          }
+        })
+      }, [request])
+
+      if (error) {
+        return (
+          <p>{error.message}</p>
+        )
+      }
+
+      return (
+        <p>{state}</p>
+      )
+    }
+
+    const { container } = render(<Main />)
+
+    await new Promise(resolve => setTimeout(resolve, 1))
+
+    expect(container.innerText).toEqual("123")
 
     spy.mockClear()
   })
