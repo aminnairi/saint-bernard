0a54ad3d51099144c0c2228decabce9e538a57a3
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-06-29 15:11:28 +0200

    updated according to the recent changes

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useCallback, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import { CancelError, useRequest } from "../hooks"
+import { CancelError, useStatefulRequest } from "../hooks"
 import { z } from "zod"
 
 const usersSchema = z.array(z.object({
@@ -17,21 +17,29 @@ type Users = z.infer<typeof usersSchema>
 export const UsersPage = () => {
   const navigate = useNavigate()
 
-  const { data, error, loading, request, cancel } = useRequest<Users>({
-    initialData: [],
-    initialOptions: {},
-    initialPath: "users",
-    initialUrl: "https://jsonplaceholder.typicode.com",
-    initialQueries: {},
-    resolver: async (response) => {
-      const users = await response.json()
-      return usersSchema.parse(users)
-    }
+  const { state, error, loading, request, cancel } = useStatefulRequest<Users>({
+    initialState: [],
   })
 
   const goTo = useCallback((path: string) => () => {
     navigate(path)
   }, [navigate])
+
+  const getUsers = useCallback(() => {
+    request({
+      url: "https://jsonplaceholder.typicode.com/users",
+      method: "GET",
+      headers: {
+        "Accept": "application/json"
+      },
+      onResponse: async response => {
+        const users = await response.json()
+        const parsed = usersSchema.parse(users)
+
+        return parsed
+      }
+    })
+  }, [])
 
   useEffect(() => cancel, [cancel])
 
@@ -51,7 +59,7 @@ export const UsersPage = () => {
         <Fragment>
           <h1>Canceled</h1>
           <p>You have canceled the request.</p>
-          <button onClick={request}>Retry?</button>
+          <button onClick={getUsers}>Retry?</button>
         </Fragment>
       )
     }
@@ -61,7 +69,7 @@ export const UsersPage = () => {
         <h1>Error</h1>
         <p>Something went wrong</p>
         <small>{error.message}</small>
-        <button onClick={request}>Retry?</button>
+        <button onClick={getUsers}>Retry?</button>
       </Fragment>
     )
   }
@@ -69,38 +77,38 @@ export const UsersPage = () => {
   return (
     <Fragment>
       <h1>Users</h1>
-      <button onClick={request}>Fetch users</button>
-      {data.length === 0 && (
+      <button onClick={getUsers}>Fetch users</button>
+      {state.length === 0 && (
         <p>No users to show</p>
       )}
-    {data.length !== 0 && (
-      <table>
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>Name</th>
-            <th>Username</th>
-            <th>Phone</th>
-            <th>Website</th>
-            <th>Posts</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(user => (
-            <tr key={user.id}>
-              <td>{user.email}</td>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-              <td>{user.phone}</td>
-              <td>{user.website}</td>
-              <td>
-                <button onClick={goTo(`/users/${user.id}/posts`)}>See posts</button>
-              </td>
+      {state.length !== 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Phone</th>
+              <th>Website</th>
+              <th>Posts</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    )}
+          </thead>
+          <tbody>
+            {state.map(user => (
+              <tr key={user.id}>
+                <td>{user.email}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.phone}</td>
+                <td>{user.website}</td>
+                <td>
+                  <button onClick={goTo(`/users/${user.id}/posts`)}>See posts</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </Fragment>
   )
 }
