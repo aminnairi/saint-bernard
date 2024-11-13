b1416e9f133a32043365818ddcff9b14c406148d
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-11-13 10:45:46 +0100

    moved the main component in its workspace

--- apps/example/main.tsx
+++ apps/example/main.tsx
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react"
 import { Routes, Route } from "react-router-dom"
-import { UsersUserPostsPage } from "./pages/users/user/posts"
-import { HomePage } from "./pages/home"
-import { UsersPage } from "./pages/users"
-import { Header } from "./components/header"
-import { UserPage } from "./pages/users/user"
+import { UsersUserPostsPage } from "./src/pages/users/user/posts"
+import { HomePage } from "./src/pages/home"
+import { UsersPage } from "./src/pages/users"
+import { Header } from "./src/components/header"
+import { UserPage } from "./src/pages/users/user"
 
 export const Main = () => {
   return (
